662bf23ea08deec8881321e949fddc612291a8d4
Syed Daniyal Ahmed <syeddani23.99@gmail.com>
2023-04-06 14:43:12 +0500

    Styling in Process

--- Part-9 JavaScript in the Browser DOM and Events Fundamentals/Guess Number/tailwind.config.js
+++ Part-9 JavaScript in the Browser DOM and Events Fundamentals/Guess Number/tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
+    colors: {
+      Primary: "#EE5921",
+      Secondary: "#242A38",
+      DarkGrey: "#4E586E",
+      White: "#FFFFFF",
+    },
     extend: {},
   },
   plugins: [],
